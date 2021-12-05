f08aa1753e7f3a3d66b23bcdd2344e737b9b4a36
Tania Guillot <tguillot2000@gmail.com>
2021-12-05 14:26:03 +0100

    web page main structures

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,21 +19,33 @@ public class Application extends Controller {
 
     public static void index() {
         render();
-        }
+    }
 
     public static void login() {
         render();
     }
 
-    public static void signup(){
+    public static void signup() {
         render();
     }
 
-    public static void registered(@Valid User user, String verifyPassword){
-        //validation.required(user.name);
+    public static void api1() {
+        render();
+    }
+
+    public static void imageOfTheDay() {
+        // save the image of the day for the current user
+    }
+
+    public static void dataApi1() {
+        // save the data of api 1 for user1
+    }
+
+    public static void registered(@Valid User user, String verifyPassword) {
+        // validation.required(user.name);
         validation.required(verifyPassword);
         validation.equals(verifyPassword, user.password).message("Your password doesn't match");
-        if(validation.hasErrors()) {
+        if (validation.hasErrors()) {
             render("@signup", user, verifyPassword);
         }
         user.create();
@@ -41,143 +53,128 @@ public class Application extends Controller {
         flash.success("Welcome, " + user.name);
         index();
     }
-    public static void start(String uname, String psw, boolean unregister){
-        User u = User.find("byName",uname).first();
+
+    public static void start(String uname, String psw, boolean unregister) {
+        User u = User.find("byName", uname).first();
         if (u != null) {
-            if (u.password.equals(psw))
-            {
-                if(unregister){
+            if (u.password.equals(psw)) {
+                if (unregister) {
                     u.delete();
                     index();
-                }else {
+                } else {
                     render();
                 }
-            }
-            else{
+            } else {
                 flash.error("Incorrect password");
                 login();
             }
-        }
-        else {
+        } else {
             flash.error("User does not exists");
             login();
         }
     }
 
-    public static void editPassword(String uname, String psw, String newPsw){
-        //http://localhost:9000/Application/editPassword?uname=a&psw=a&newPsw=aaa
-        User u = User.find("byName",uname).first();
+    public static void editPassword(String uname, String psw, String newPsw) {
+        // http://localhost:9000/Application/editPassword?uname=a&psw=a&newPsw=aaa
+        User u = User.find("byName", uname).first();
         List<User> users = new ArrayList<>();
-        for (User user:users ) {
+        for (User user : users) {
 
         }
         if (u != null) {
-            if (u.password.equals(psw))
-            {
-                u.password=newPsw;
+            if (u.password.equals(psw)) {
+                u.password = newPsw;
                 u.save();
                 renderText("password changed");
 
-            }
-            else{
+            } else {
                 renderText("Incorrect password");
             }
-        }
-        else {
+        } else {
             renderText("User does not exists");
 
         }
     }
 
-    public static void editProfilePic(String uname, String newProfilePic){
-        //http://localhost:9000/Application/editProfilePic?uname=a&newPorfilePic=dexter1
-        User u = User.find("byName",uname).first();
+    public static void editProfilePic(String uname, String newProfilePic) {
+        // http://localhost:9000/Application/editProfilePic?uname=a&newPorfilePic=dexter1
+        User u = User.find("byName", uname).first();
         if (u != null) {
-            u.profilePic=newProfilePic;
+            u.profilePic = newProfilePic;
             u.save();
             renderText("profile pic changed");
-        }
-        else {
+        } else {
             renderText("User does not exists");
         }
 
     }
 
-    public static void addPicture(String uname,@As("dd/MM/yyyy") Date pictureDate) throws ParseException {
-        //http://localhost:9000/Application/addPicture?uname=a&pictureDate=12/12/2000
-        Picture pic = Picture.find("byDate",pictureDate).first();
-        User user = User.find("byName",uname).first();
+    public static void addPicture(String uname, @As("dd/MM/yyyy") Date pictureDate) throws ParseException {
+        // http://localhost:9000/Application/addPicture?uname=a&pictureDate=12/12/2000
+        Picture pic = Picture.find("byDate", pictureDate).first();
+        User user = User.find("byName", uname).first();
         if (user != null) {
-            if(pic==null){
+            if (pic == null) {
                 pic = new Picture(pictureDate);
             }
-            if(!user.pictures.contains(pic)) {
+            if (!user.pictures.contains(pic)) {
                 pic.users.add(user);
                 pic.save();
                 user.pictures.add(pic);
                 user.save();
                 renderText("added user");
             }
-        }
-        else {
+        } else {
             renderText("User does not exists");
         }
 
     }
 
-    //Android functions
-    public void androidLogin(String uname, String psw){
-        User u = User.find("byName",uname).first();
+    // Android functions
+    public void androidLogin(String uname, String psw) {
+        User u = User.find("byName", uname).first();
         JsonObject jsonObject = new JsonObject();
         if (u != null) {
-            if (u.password.equals(psw))
-            {
-                jsonObject.addProperty("login",true);
+            if (u.password.equals(psw)) {
+                jsonObject.addProperty("login", true);
+                renderJSON(jsonObject);
+            } else {
+                jsonObject.addProperty("login", false);
                 renderJSON(jsonObject);
             }
-            else{
-                jsonObject.addProperty("login",false);
-                renderJSON(jsonObject);
-            }
-        }
-        else {
-            jsonObject.addProperty("login",false);
+        } else {
+            jsonObject.addProperty("login", false);
             renderJSON(jsonObject);
         }
     }
 
-    //Android functions
-    public void andriodRegister(String name, String password,String verifyPassword, int age, String dexter ){
+    // Android functions
+    public void andriodRegister(String name, String password, String verifyPassword, int age, String dexter) {
         JsonObject jsonObject = new JsonObject();
-        User u = User.find("byName",uname).first();
-        //Check if user already exists
-        if(u==null){
-            //Check that the passwords match
-            if(password==verifyPassword){
+        User u2 = User.find("byName", name).first();
+        // Check if user already exists
+        if (u2 == null) {
+            // Check that the passwords match
+            if (password == verifyPassword) {
                 User u = new User(name, password, dexter, age);
                 u.save();
-                jsonObject.addProperty("register",true);
+                jsonObject.addProperty("register", true);
                 renderJSON(jsonObject);
-            } else{
-                jsonObject.addProperty("register",false);
-                jsonObject.addProperty("error", "Passwords don't match")
+            } else {
+                jsonObject.addProperty("register", false);
+                jsonObject.addProperty("error", "Passwords don't match");
                 renderJSON(jsonObject);
-            } 
+            }
         } else {
-                jsonObject.addProperty("register",false);
-                jsonObject.addProperty("error", "User already exists")
-                renderJSON(jsonObject);
+            jsonObject.addProperty("register", false);
+            jsonObject.addProperty("error", "User already exists");
+            renderJSON(jsonObject);
         }
     }
 
-
-
-    //Test functions
-    public void getSomething(){
+    // Test functions
+    public void getSomething() {
         renderJSON("{\"phonetype\":\"N95\"}");
     }
-
-
-
 
 }
